e6167490731ec10dda7c358d243468e91c1221e7
SouOWendel <SouOWendel@gmail.com>
2026-09-21 20:50:23 -0300

    aula-0914

---
+++ src/main/java/br/edu/ifsp/orderflow/events/PagamentoAprovado.java
@@ -0,0 +1,16 @@
+package br.edu.ifsp.orderflow.events;
+
+import java.time.Instant;
+
+/**
+ * Evento que será publicado quando um pagamento for aprovado.
+ *
+ * @param pedidoId
+ * @param transacaoId
+ * @param ocorridoEm
+ */
+public record PagamentoAprovado(
+        String pedidoId,
+        String transacaoId,
+        Instant ocorridoEm
+) implements IDomainEvent {}
